82fd58c1713b79b45332d2a2168ea8bc4626fe43
Arnold Kalmbach <akalmbach@cim.mcgill.ca>
2014-01-09 16:33:06 -0500

    Added single subscriber and styling example

--- js/sub.js
+++ js/sub.js
@@ -10,10 +10,45 @@ var ros = new ROSLIB.Ros();
 var addr = "localhost:9090";
 var connected = false;
 var tracker = new Tracker('red');
+var msgps = 10;
 
+
+function plotGPS(tracker, msg) {
+  var GPSMsgTime = new Date();
+
+  if ((GPSMsgTime - tracker.prevGPSMsgTime) < 1000/msgps) {
+    return;
+  }
+  
+  tracker.prevGPSMsgTime = GPSMsgTime;
+  var lat = msg.latitude;
+  var lon = msg.longitude;
+  var time = msg.header.stamp.secs + msg.header.stamp.nsecs/1e9;
+  var str = time.toString() + ' @ (' + lat.toString() + ',' + lon.toString() + ')';
+
+  console.log('recv gps ' + tracker.color + ' (' + lat.toString() + ',' + lon.toString() + ') @ ' + time.toString());
+
+  var maxNumMarkers = parseInt($('#path_length').val());
+  if (maxNumMarkers > 0) {
+    while (tracker.markers.length >= maxNumMarkers) {
+      var removedMarker = tracker.markers.shift();
+      removedMarker.setMap(null);
+    }
+  }
+  
+  manualpt = new OpenLayers.Feature.Vector(
+    backTransform(new OpenLayers.Geometry.Point(lon,lat))
+  );
+  manualpt.style = OpenLayers.Util.applyDefaults({fillColor: tracker.color, strokeColor: "black"},
+    OpenLayers.Feature.Vector.style["default"]
+  ); 
+  vectorLayer.addFeatures([manualpt]);
+  
+}
 
 function callbackGPS(msg) {
-  console.log(msg);
+  //console.log(msg);
+  plotGPS(tracker, msg);
 }
 
 function toggleConnection() {  
